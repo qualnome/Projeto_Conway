cb19d6ebdfde583224cf5f133f3cb59f8f7315d1
qualnome <76534747+qualnome@users.noreply.github.com>
2020-12-23 19:23:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #Sitio para escrevermos o codigo
+def cria_mundo (linhas,colunas):
+    l= []
+    l2 = []
+    for x in range (colunas):      
+        l += [0,]    
+    for x in range (linhas):       
+        l2.append(l)
+    return(l2)
+ 
+l2=cria_mundo(5,5)
+def tamanho_mundo (l2):
+    tuplo=(len(l2),)
+    tuplo +=(len(l2),)
+    return(tuplo)
+print(tamanho_mundo(l2))
+ 
